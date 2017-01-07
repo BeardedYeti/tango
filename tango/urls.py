1dd771158263136e95dd8d298825b1b8d9cff6a2
Colin Smith <cmsgoboston41@gmail.com>
2017-01-07 18:44:46 -0500

    Add Rango routing

--- tango/urls.py
+++ tango/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
 from rango import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
+	# Handles any URL with rango/ handled by rango application
+	url(r'^rango/', include('rango.urls')),
     url(r'^admin/', admin.site.urls),
 ]
