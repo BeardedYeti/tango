73a614947a1d599def8749ba7cc5c453d10c327f
Colin Smith <cmsgoboston41@gmail.com>
2017-01-11 07:55:51 -0500

    Add serving media files functionality

--- tango/settings.py
+++ tango/settings.py
@@ -15,7 +15,11 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -54,8 +58,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'tango.urls'
 
-TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -67,6 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media'
             ],
         },
     },
@@ -74,6 +77,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'tango.wsgi.application'
 
+MEDIA_ROOT = MEDIA_DIR
+
+MEDIA_URL = '/media/'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
--- tango/urls.py
+++ tango/urls.py
@@ -17,10 +17,12 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	# Handles any URL with rango/ handled by rango application
 	url(r'^rango/', include('rango.urls')),
     url(r'^admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
